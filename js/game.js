a5e676d00fc4c4c81b4807d09ff58fc17d554d4f
example237 <michael.gampmayer@gmail.com>
2026-01-03 14:52:34 +0100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,35 +1,20 @@
-class NameScene extends Phaser.Scene {
-    constructor() {
-        super('NameScene');
-    }
-    create() {
-        this.add.text(200, 200, 'NameScene läuft', {
-            font: '32px Arial',
-            fill: '#ffffff'
-        });
+window.onload = function () {
 
-        this.time.delayedCall(2000, () => {
-            this.scene.start('GameScene');
-        });
+    class TestScene extends Phaser.Scene {
+        create() {
+            this.add.text(100, 200, 'Phaser läuft!', {
+                font: '32px Arial',
+                fill: '#ffffff'
+            });
+        }
     }
-}
 
-class GameScene extends Phaser.Scene {
-    constructor() {
-        super('GameScene');
-    }
-    create() {
-        this.add.text(200, 200, 'GameScene läuft', {
-            font: '32px Arial',
-            fill: '#00ff00'
-        });
-    }
-}
-const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 450,
-    backgroundColor: '#000000',
-    scene: ['NameScene', 'GameScene']
+    new Phaser.Game({
+        type: Phaser.AUTO,
+        width: 800,
+        height: 450,
+        backgroundColor: '#000000',
+        scene: TestScene
+    });
+
 };
-new Phaser.Game(config);
